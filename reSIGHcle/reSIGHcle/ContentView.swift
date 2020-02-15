d44988c4f43ca10e7c2a100a1a57f81ac76eaa3f
Soumya Duriseti <sbduriseti@ucdavis.edu>
2020-02-15 01:18:18 -0800

    created app

---
+++ reSIGHcle/reSIGHcle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  reSIGHcle
+//
+//  Created by Soumya Duriseti on 2/15/20.
+//  Copyright © 2020 live_our_best_life. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ reSIGHcle/reSIGHcleTests/reSIGHcleTests.swift
@@ -0,0 +1,34 @@
+//
+//  reSIGHcleTests.swift
+//  reSIGHcleTests
+//
+//  Created by Soumya Duriseti on 2/15/20.
+//  Copyright © 2020 live_our_best_life. All rights reserved.
+//
+
+import XCTest
+@testable import reSIGHcle
+
+class reSIGHcleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
